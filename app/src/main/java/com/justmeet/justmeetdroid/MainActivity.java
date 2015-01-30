2f3002f3425e608b517285394c8face14cceef45
Prakash Iyer <prakashk16@gmail.com>
2015-01-31 03:35:09 +0530

    test commit

--- app/src/main/java/com/justmeet/justmeetdroid/MainActivity.java
+++ app/src/main/java/com/justmeet/justmeetdroid/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            System.out.println("New Project");
             return rootView;
         }
     }
